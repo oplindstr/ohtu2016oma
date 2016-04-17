b2738b24fef5ddcffed81ab1b3e7824a5cef2e0b
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2016-04-17 21:58:42 +0300

    viikko2 masteriin

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,16 +1,23 @@
 package ohtu.verkkokauppa;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Viitegeneraattori implements Viiterajapinta {
+public class Viitegeneraattori {
+
+    private static Viitegeneraattori instanssi;
+
+    public static Viitegeneraattori getInstance() {
+        if (instanssi == null) {
+            instanssi = new Viitegeneraattori();
+        }
+
+        return instanssi;
+    }
     
     private int seuraava;
     
-    public Viitegeneraattori(){
+    private Viitegeneraattori(){
         seuraava = 1;    
     }
     
-    @Override
     public int uusi(){
         return seuraava++;
     }
